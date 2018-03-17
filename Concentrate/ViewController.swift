7df923f8423dc9f0ca3a62acc8d26b1a11ab4aeb
Tian Qin <root1@FicusdeMacBook-Pro.local>
2018-03-17 15:13:59 +0800

    Initial Commit

---
+++ Concentrate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentrate
+//
+//  Created by Ficus carica Linn on 17/03/2018.
+//  Copyright © 2018 Ficus carica Linn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
